68d2daef71b17d6a405f86d2c065f0ae0d2157ab
Floyd Corbin <floyd.corbin@hoodoo.digital>
2023-12-22 18:21:38 -0700

    updates from other repository

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# FloydCorbin.github.io Page
-https://floydcorbin.github.io/
-
-## Checkout my personal site to learn more abour me and my interests, skills and other things. 
+# GitHub-Pages-Portfolio
--- logic.js
+++ logic.js
@@ -1,6 +1,12 @@
 //capture the scroll event on the page and hide the image with the id of "logo" when the scroll position is greater than 5px
 $(window).scroll(function () {
-  if ($(document).scrollTop() > 1) {
+  if ($(document).scrollTop() > 1%) {
     $(".header").fadeOut(900);
+  }
+  if ($(document).scrollTop() > 99%) {
+  //    //change the .nav position to fixed so that the nav bar stays at the top of the page
+  //    $(".nav").css("position", "fixed");
+     $(".nav").css("top", "0");
+     $(".nav").css("left", "0");
   }
 });
